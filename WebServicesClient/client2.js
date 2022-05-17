c8fad1f32be0429264d09f540d1735881ed2d718
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2022-05-17 13:05:12 +0700

    Update Lab11

---
+++ WebServicesClient/client2.js
@@ -0,0 +1,16 @@
+var XMLHttpRequest = require('xhr2');
+
+const getTodos = () => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log("Không nhận được dữ liệu từ server!");
+        }
+    });
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+getTodos();
